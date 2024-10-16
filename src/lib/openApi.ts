c39b7229f854d8a8431990feed65a0449c5773ab
radekskrabucha <rskrabucha13@gmail.com>
2024-10-16 19:09:12 +0200

    update package.json, tsconfig, and OpenAPI config; change start command, enhance build process, and resolve module paths

--- src/lib/openApi.ts
+++ src/lib/openApi.ts
@@ -1,14 +1,14 @@
 import { swaggerUI } from '@hono/swagger-ui'
 import type { AppOpenApi } from '~/types/app'
-import { version, name } from '../../package.json'
+import packageJson from '../../package.json' with { type: "json" }
 
 export const configureOpenApi = (app: AppOpenApi) => {
   // The OpenAPI documentation will be available at /doc
   app.doc('/doc', {
     openapi: '3.0.0',
     info: {
-      version,
-      title: name
+      version: packageJson.version,
+      title: packageJson.name
     }
   })
   app.get('/swagger', swaggerUI({ url: '/api/doc' }))
